7eef475eaa621a32aef74cb8741fcc35015faa49
Pieterjan Verhelst <Pieterjan.Verhelst@UGent.be>
2017-12-07 16:54:08 +0100

    check if condition differs according to maturation stadium

--- src/Analysis_condition.R
+++ src/Analysis_condition.R
@@ -1,13 +1,22 @@
-# Analyse if condition factor K width differs according to head width within maturity classes
+# Analyse if condition factor K  differs according to head width within maturity classes
 # By Pieterjan Verhelst
 
 
+# First check if K differs according to maturation stadia
+boxplot(eels$K~eels$Stadium, ylab = "K") 
+aov <- aov(eels$K ~ eels$Stadium)
+plot(aov)  # Check assumptions
+summary(aov)
+
+TukeyHSD(aov, conf.level=0.95, ordered = FALSE)  # sign difference between some groups, so nested design
+
+
 # Create plot
-eels$Stadium <- factor(eels$Stadium)
-boxplot(eels$K~eels$HW/eels$Stadium, ylab = "K")
+boxplot(eels$K~eels$class, ylab = "K")
+boxplot(eels$K~eels$class/eels$Stadium, ylab = "K")
 
 # Apply anova with nested design (HW is nested in stadium)
-aov <- aov(eels$K ~ eels$HW/eels$Stadium)
+aov <- aov(eels$K ~ eels$class/eels$Stadium)
 plot(aov)  # Check assumptions
 summary(aov)
 
