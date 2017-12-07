f050869b765ddd4e7f9146875434015d766aa31e
Pieterjan Verhelst <Pieterjan.Verhelst@UGent.be>
2017-12-07 13:52:39 +0100

    calculate ratio head width - head length

--- src/Link_headwidth.R
+++ src/Link_headwidth.R
@@ -1,9 +1,24 @@
 # Link head width data to dataset
 # By Pieterjan Verhelst
 
+library(dplyr)
 
 # Upload eel data
 hw <- read.csv("./data/raw/Headdimorphism.csv",sep=";",stringsAsFactors = FALSE)
+
+# Select relevant columns, substitute ',' by '.' and set as numeric
+hw <- select(hw, ID, Width.skull, Head.length)
+hw$Width.skull <- gsub("\\,", ".", hw$Width.skull)
+hw$Width.skull <- as.numeric(hw$Width.skull)
+hw$Head.length <- gsub("\\,", ".", hw$Head.length)
+hw$Head.length <- as.numeric(hw$Head.length)
+
+# Rename columns
+colnames(hw)[2] <- "HW"   # Width.skull is head width = HW
+colnames(hw)[3] <- "HL"   # Head.length is head length = HL
+
+# Calculate ratio HW:HL
+hw$HWHL <- hw$HW/hw$HL
 
 # Merge two datasets
 eels <- merge(eels, hw, by="ID")
