aeefdb42e4313c80792cc42f95da76e55f557f71
Pieterjan Verhelst <Pieterjan.Verhelst@UGent.be>
2017-12-07 13:57:01 +0100

    remove redundant code

--- src/Link_headwidth.R
+++ src/Link_headwidth.R
@@ -23,21 +23,4 @@ hw$HWHL <- hw$HW/hw$HL
 # Merge two datasets
 eels <- merge(eels, hw, by="ID")
 
-# Remove columns
-eels$lengte..mm. <- NULL
-eels$voor.oog..mm.<- NULL
-eels$achter.oog..mm. <- NULL
-
-# Change column name
-colnames(eels)[15] <- "Before_eye"
-colnames(eels)[16] <- "Behind_eye"
-colnames(eels)[17] <- "HW"
-
-# Substitute comma by dot in weight column
-eels$Before_eye<-gsub(",", ".", eels$Before_eye)
-eels$Behind_eye<-gsub(",", ".", eels$Behind_eye)
-
-# Set weight and index as numeric
-eels$Before_eye <- as.numeric(eels$Before_eye)
-eels$Behind_eye <- as.numeric(eels$Behind_eye)
 
