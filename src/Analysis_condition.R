ef49bd21e8ebac0d565bef3bea691195fb095f56
Pieterjan Verhelst <Pieterjan.Verhelst@UGent.be>
2018-02-06 15:32:31 +0100

    conduct kw test and calculate median

--- src/Analysis_condition.R
+++ src/Analysis_condition.R
@@ -2,6 +2,8 @@
 # By Pieterjan Verhelst
 
 
+library(PMCMR)
+
 # First check if K differs according to maturation stadia
 boxplot(eels$K~eels$Stadium, ylab = "K") 
 aov <- aov(eels$K ~ eels$Stadium)
@@ -14,6 +16,8 @@ TukeyHSD(aov, conf.level=0.95, ordered = FALSE)  # sign difference between some 
 # Calculate mean condition per head width class
 aggregate(eels$K, list(eels$class), mean)
 aggregate(eels$K, list(eels$class), sd)
+aggregate(eels$K, list(eels$class), median)
+
 
 # Create plot
 boxplot(eels$K~eels$class, ylab = "K")
@@ -47,7 +51,18 @@ fig_K_class
 
 
 
+##################
 # Apply anova
+##################
+
+# Check assumptions:
+# Normality if p-value > 0.05 (H0: normality)
+shapiro.test((eels$K)^2)
+
+# Homogeneity of variances
+leveneTest(eels$K, eels$class)
+
+
 aov <- aov(eels$K ~ eels$class)
 #aov <- aov(eels$K ~ eels$class/eels$Stadium)     # Stadium is nested within class
 plot(aov)  # Check assumptions
@@ -55,4 +70,61 @@ summary(aov)
 
 # Apply post-hoc test
 TukeyHSD(aov, conf.level=0.95, ordered = FALSE)
+
+
+##################
+# KRUSKAL-WALLIS
+##################
+
+kruskal.test(K~class, data=eels)
+
+posthoc.kruskal.dunn.test(x=eels$K, g=eels$class, p.adjust.method="bonferroni")
+
+PT = dunnTest(migration_distance ~ Month,
+              data=dist,
+              method="bh")    # Can adjust p-values;
+# See ?p.adjust for options
+
+
+
+
+
+
+
+
+# Remove outliers
+c <- eels[which(eels$class == "NH"),]
+
+x <- c(
+1.6080927,
+0.5160324,
+0.6338795,
+1.4124529,
+1.3933317,
+1.3470251,
+1.3418751,
+0.5352319,
+0.5502950,
+0.6337044,
+0.7194591,
+0.7200877,
+0.7260090,
+1.5027151,
+0.5149608)
+
+eels2 <- eels[!eels$Kround %in% x,]
+
+
+boxplot(eels2$K~eels2$class, ylab = "K")
+# Apply anova
+aov <- aov(eels2$K ~ eels2$class)
+#aov <- aov(eels$K ~ eels$class/eels$Stadium)     # Stadium is nested within class
+plot(aov)  # Check assumptions
+summary(aov)
+
+# Apply post-hoc test
+TukeyHSD(aov, conf.level=0.95, ordered = FALSE)
+
+
+
 
